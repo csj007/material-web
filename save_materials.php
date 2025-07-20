18668582acf952b75a0baa4742afb6528c5865b0
AtlasZ <651570477@qq.com>
2025-07-20 17:01:31 +0800

    Update save_materials.php

--- save_materials.php
+++ save_materials.php
@@ -1,5 +1,8 @@
 <?php
-// save_materials.php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
+
 $data = json_decode(file_get_contents('php://input'), true);
 
 if (is_array($data)) {
